eca6d7962afd985d6dfb7c06cf9b991f1e9717b6
seoyunZZang <p815000@naver.com>
2024-07-28 16:49:08 +0900

    변수 종류 공부

--- FirstTest/ContentView.swift
+++ FirstTest/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let name : String = "서윤"
-    var age : Int = 25
+    let name : String = "서윤" // 상수
+    var age : Int = 25 // 변수
+    var height: Double = 171.4
+    let hasJob : Bool = true
     
     var body: some View {
         VStack{ // vertical 로 쌓는다는 뜻
             Text(name)
             Text("나이는 \(age) 살")
+            Text("키는 \(height)")
+            Text("직장은 \(hasJob)")
         }
     }
 }
